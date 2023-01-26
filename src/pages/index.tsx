1effc91b54e92de4e6f346474beb4f9a6a83a7e3
Tchi <tchi.devica@gmail.com>
2023-01-27 00:01:29 +0300

    clerk integration

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,13 @@
 import React from "react";
 import { type NextPage } from "next";
+import { SignIn } from "@clerk/nextjs";
 
 
 const Home: NextPage = () => {
 
   return (
-    <div>
-      <div>
-        <button>
-          Google
-        </button>
-      </div>
-      <div>
-        <button>
-          Facebook
-        </button>
-      </div>
+    <div className="flex justify-center items-center h-screen w-screen">
+      <SignIn />
     </div>
   );
 };
