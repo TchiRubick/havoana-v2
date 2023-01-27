c8fac1605829709524e69701e6a5eec8d031c0d4
anderson suiriex <andersonsuiriex@gmail.com>
2023-01-27 23:56:29 +1300

    test2

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -4,8 +4,8 @@ import { type NextPage } from "next";
 const Cashier: NextPage = () => {
 
   return (
-    <div className="box-content h-32 w-32 p-4 border-4 ...">
-      a
+    <div className="">
+      
     </div>
   );
 };
