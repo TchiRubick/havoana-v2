72f42c76b5978507ddc106cb77da1be67f23b03b
anderson suiriex <andersonsuiriex@gmail.com>
2023-01-27 23:30:04 +1300

    test form

---
+++ src/pages/test.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { type NextPage } from "next";
+
+const Cashier: NextPage = () => {
+
+  return (
+    <div className="box-content h-32 w-32 p-4 border-4 ...">
+      a
+    </div>
+  );
+};
+
+export default Cashier;
