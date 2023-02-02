6b118d96e84069dc37e064578524fdbe1cc12cfe
anderson suiriex <andersonsuiriex@gmail.com>
2023-02-02 23:50:41 +1300

    inventory

--- src/pages/createinventory.tsx
+++ src/pages/createinventory.tsx
@@ -40,11 +40,7 @@ const Createinventory: NextPage = () => {
    }
   }
   
- 
   
- 
-
-
   return (
   <div className="  flex justify-center items-center h-screen w-screen">
     <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
